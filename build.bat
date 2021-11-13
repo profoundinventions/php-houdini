5677e9af1bd4018b3cccd65f36bbabf526cc4252
David Meybohm <dmeybohm@gmail.com>
2021-11-12 23:38:07 -0500

    Update

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 @echo off
 call make html
+@mkdir docs
 cp -av _build/html/* docs/
 touch docs/.nojekyll
 
--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 make html &&
     (cd _build/html/* && rm -rf *) && 
+    mkdir -p docs &&
     cp -av _build/html/* docs/ &&
     touch docs/.nojekyll
 
